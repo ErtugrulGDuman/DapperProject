f6f817b1146c6aa2ea3f2a2bdef211c0caf7370e
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-06-28 21:41:47 +0300

    Category Service added

--- DapperProject/Program.cs
+++ DapperProject/Program.cs
@@ -1,6 +1,13 @@
+using DapperProject.Context;
+using DapperProject.Services.CategoryServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<DapperContext>();
+
+builder.Services.AddScoped<ICategoryService, CategoryServices>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
